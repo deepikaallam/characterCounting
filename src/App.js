1ea89d8317e712aefb7659ff802941b39b90c3eb
deepikaallam <deepika.allam1311@gamail.com>
2023-04-14 14:45:32 +0000

    chacters counting

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+import CharacterItem from './components/CharacterItem'
+import './App.css'
+
+// Replace your code here
+class App extends Component {
+  state = {
+    inputWord: '',
+    characterList: [],
+    isAddButtonClicked: false,
+  }
+
+  onChangeInput = event => {
+    const {value} = event.target
+    this.setState({inputWord: value, isAddButtonClicked: true})
+  }
+
+  onAdd = event => {
+    event.preventDefault()
+    const {inputWord} = this.state
+    const newWord = {
+      id: v4(),
+      word: inputWord,
+      isAddButtonClicked: false,
+    }
+    this.setState(prevState => ({
+      characterList: [...prevState.characterList, newWord],
+      inputWord: '',
+    }))
+  }
+
+  render() {
+    const {inputWord, isAddButtonClicked, characterList} = this.state
+    return (
+      <div className="character-main-container">
+        <div className="character-sub-container">
+          <h1 className="main-heading">Count the characters like a Boss</h1>
+          {isAddButtonClicked ? (
+            <div>
+              <ul>
+                {characterList.map(each => (
+                  <CharacterItem key={each.id} characterItemDetails={each} />
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/no-user-inputs-img.png"
+              alt="no user inputs"
+              className="image"
+            />
+          )}
+        </div>
+        <div className="character-sub-container-1">
+          <h1 className="sub-heading">Character Counter</h1>
+          <div className="input-container">
+            <form onSubmit={this.onAdd}>
+              <input
+                type="text"
+                placeholder="Enter the Characters here"
+                className="input-element"
+                value={inputWord}
+                onChange={this.onChangeInput}
+              />
+              <button type="submit" className="add-button">
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/CharacterItem/index.js
@@ -0,0 +1,12 @@
+const CharacterItem = props => {
+  const {characterItemDetails} = props
+  const {word} = characterItemDetails
+  return (
+    <li>
+      <p>
+        {word}:{word.length}
+      </p>
+    </li>
+  )
+}
+export default CharacterItem
